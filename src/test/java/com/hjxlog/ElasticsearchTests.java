bd7d9619db96ac93a8c7082fab4049d2cc3b7957
HuangJianxian <jxHuang@kingdomai.com>
2022-02-14 20:04:34 +0800

    update

--- src/test/java/com/hjxlog/ElasticsearchTests.java
+++ src/test/java/com/hjxlog/ElasticsearchTests.java
@@ -1,9 +1,36 @@
 package com.hjxlog;
 
+import cn.hutool.json.JSON;
+import cn.hutool.json.JSONUtil;
+import com.hjxlog.domain.User;
+import org.elasticsearch.action.admin.indices.delete.DeleteIndexRequest;
+import org.elasticsearch.action.bulk.BulkRequest;
+import org.elasticsearch.action.bulk.BulkResponse;
+import org.elasticsearch.action.delete.DeleteRequest;
+import org.elasticsearch.action.delete.DeleteResponse;
+import org.elasticsearch.action.get.GetRequest;
+import org.elasticsearch.action.get.GetResponse;
+import org.elasticsearch.action.index.IndexRequest;
+import org.elasticsearch.action.index.IndexResponse;
+import org.elasticsearch.action.search.SearchRequest;
+import org.elasticsearch.action.search.SearchResponse;
+import org.elasticsearch.action.support.master.AcknowledgedResponse;
+import org.elasticsearch.action.update.UpdateRequest;
+import org.elasticsearch.action.update.UpdateResponse;
 import org.elasticsearch.client.RequestOptions;
 import org.elasticsearch.client.RestHighLevelClient;
 import org.elasticsearch.client.indices.CreateIndexRequest;
 import org.elasticsearch.client.indices.CreateIndexResponse;
+import org.elasticsearch.client.indices.GetIndexRequest;
+import org.elasticsearch.common.unit.TimeValue;
+import org.elasticsearch.common.xcontent.XContentType;
+import org.elasticsearch.index.query.QueryBuilders;
+import org.elasticsearch.index.query.TermQueryBuilder;
+import org.elasticsearch.search.SearchHit;
+import org.elasticsearch.search.SearchHits;
+import org.elasticsearch.search.builder.SearchSourceBuilder;
+import org.elasticsearch.search.fetch.subphase.FetchSourceContext;
+import org.elasticsearch.search.fetch.subphase.highlight.HighlightBuilder;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -11,6 +38,8 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 import javax.annotation.Resource;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @Author: Huang JX
@@ -33,4 +62,151 @@ public class ElasticsearchTests {
         System.out.println(response);// 查看返回对象
         restHighLevelClient.close();
     }
+
+    // 测试获取索引
+    @Test
+    public void testExistIndex() throws IOException {
+        GetIndexRequest request = new GetIndexRequest("es_index");
+        boolean exists = restHighLevelClient.indices().exists(request, RequestOptions.DEFAULT);
+        System.out.println(exists);
+    }
+
+    // 测试删除索引
+    @Test
+    public void deleteIndex() throws IOException {
+        DeleteIndexRequest request = new DeleteIndexRequest("es_index");
+        AcknowledgedResponse delete = restHighLevelClient.indices().delete(request, RequestOptions.DEFAULT);
+        System.out.println(delete.isAcknowledged());
+    }
+
+
+    // 测试添加文档
+    @Test
+    public void testAddDocment() throws IOException {
+        // 创建对象
+        User user = new User("es", 18);
+        // 创建请求
+        IndexRequest request = new IndexRequest("es_index");
+        //规则 put/es_index/_doc/1
+        request.id("1");
+        request.timeout(TimeValue.timeValueSeconds(1));
+
+        // 将数据放入请求 json
+        request.source(JSONUtil.toJsonStr(user), XContentType.JSON);
+
+        // 客户端发送请求，获取响应结果
+        IndexResponse indexResponse = restHighLevelClient.index(request, RequestOptions.DEFAULT);
+        System.out.println(indexResponse.toString());
+        System.out.println(indexResponse.status());
+    }
+
+    // 获取文档，判断是否存在 get /index/_doc/1
+    @Test
+    public void testIsExistsDoc() throws IOException {
+        GetRequest getRequest = new GetRequest("es_index", "1");
+
+        // 不获取返回的_source的上下文
+        getRequest.fetchSourceContext(new FetchSourceContext(false));
+        getRequest.storedFields("_none_");
+
+        boolean exists = restHighLevelClient.exists(getRequest, RequestOptions.DEFAULT);
+        System.out.println(exists);
+
+    }
+
+    // 获取文档信息
+    @Test
+    public void testIsGetDoc() throws IOException {
+        GetRequest getRequest = new GetRequest("es_index", "1");
+        GetResponse getResponse = restHighLevelClient.get(getRequest, RequestOptions.DEFAULT);
+        System.out.println(getResponse.getSourceAsString()); // 打印文档内容
+        System.out.println(getResponse);
+    }
+
+    // 测试更新文档内容
+    @Test
+    public void testUpdateDocument() throws IOException {
+        UpdateRequest request = new UpdateRequest("es_index", "1");
+        User user = new User("hjx", 20);
+        request.doc(JSONUtil.toJsonStr(user), XContentType.JSON);
+
+        UpdateResponse response = restHighLevelClient.update(request, RequestOptions.DEFAULT);
+        System.out.println(response.status()); // OK
+        restHighLevelClient.close();
+    }
+
+    // 测试删除文档
+    @Test
+    public void testDeleteDocument() throws IOException {
+        DeleteRequest request = new DeleteRequest("es_index", "1");
+        request.timeout("1s");
+        DeleteResponse response = restHighLevelClient.delete(request, RequestOptions.DEFAULT);
+        System.out.println(response.status());// OK
+    }
+
+    // 特殊的，真的项目一般会 批量插入数据
+    @Test
+    public void testBulk() throws IOException {
+        BulkRequest bulkRequest = new BulkRequest();
+        bulkRequest.timeout("10s");
+        ArrayList<User> users = new ArrayList<>();
+        users.add(new User("hjx-1", 1));
+        users.add(new User("hjx-2", 2));
+        users.add(new User("hjx-3", 3));
+        users.add(new User("hjx-4", 4));
+        users.add(new User("hjx-5", 5));
+        users.add(new User("hjx-6", 6));
+        // 批量请求处理
+        for (int i = 0; i < users.size(); i++) {
+            bulkRequest.add(
+                    // 这里是数据信息
+                    new IndexRequest("es_index")
+                            .id("" + (i + 1)) // 没有设置id 会自定生成一个随机id
+                            .source(JSONUtil.toJsonStr(users.get(i)), XContentType.JSON)
+            );
+        }
+        BulkResponse bulk = restHighLevelClient.bulk(bulkRequest, RequestOptions.DEFAULT);
+        System.out.println(bulk.status());// ok
+    }
+
+    // 查询
+    // SearchRequest 搜索请求
+    // SearchSourceBuilder 条件构造
+    // HighlightBuilder 高亮
+    // TermQueryBuilder 精确查询
+    // MatchAllQueryBuilder
+    // xxxQueryBuilder ...
+    @Test
+    public void testSearch() throws IOException {
+        // 1.创建查询请求对象
+        SearchRequest searchRequest = new SearchRequest("es_index");
+        // 2.构建搜索条件
+        SearchSourceBuilder searchSourceBuilder = new SearchSourceBuilder();
+        // (1)查询条件 使用QueryBuilders工具类创建
+        // 精确查询
+        TermQueryBuilder termQueryBuilder = QueryBuilders.termQuery("name", "hjx");
+        //        // 匹配查询
+        //        MatchAllQueryBuilder matchAllQueryBuilder = QueryBuilders.matchAllQuery();
+        // (2)其他<可有可无>：（可以参考 SearchSourceBuilder 的字段部分）
+        // 设置高亮
+        searchSourceBuilder.highlighter(new HighlightBuilder());
+        //        // 分页
+        //        searchSourceBuilder.from();
+        //        searchSourceBuilder.size();
+        searchSourceBuilder.timeout(new TimeValue(60, TimeUnit.SECONDS));
+        // (3)条件投入
+        searchSourceBuilder.query(termQueryBuilder);
+        // 3.添加条件到请求
+        searchRequest.source(searchSourceBuilder);
+        // 4.客户端查询请求
+        SearchResponse search = restHighLevelClient.search(searchRequest, RequestOptions.DEFAULT);
+        // 5.查看返回结果
+        SearchHits hits = search.getHits();
+        System.out.println(JSONUtil.toJsonStr(hits));
+        System.out.println("=======================");
+        for (SearchHit documentFields : hits.getHits()) {
+            System.out.println(documentFields.getSourceAsMap());
+        }
+    }
+
 }
